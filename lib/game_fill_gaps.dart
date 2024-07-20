0a7a97ab02cbbcea44f7aa857c721705bf650a44
Dmitrii Polianskii <89polis@gmail.com>
2024-07-20 20:30:47 +0200

    Create a layout for first game

--- lib/game_fill_gaps.dart
+++ lib/game_fill_gaps.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class GameFillGaps extends StatefulWidget {
@@ -14,9 +15,43 @@ class _GameFillGapsState extends State<GameFillGaps> {
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.primary,
           foregroundColor: Theme.of(context).colorScheme.onPrimary,
-          title: Text("TEST",
+          title: Text("Fill the Gaps",
               style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
         ),
-        body: Text("GAME 1"));
+        body: Column(
+          children: [
+            Expanded(child: Center(child: Text("test"))),
+            GridView.count(
+              shrinkWrap: true,
+              primary: false,
+              padding: const EdgeInsets.all(4),
+              crossAxisSpacing: 0,
+              mainAxisSpacing: 0,
+              crossAxisCount: 2,
+              children: <Widget>[
+                Card(
+                  color: Theme.of(context).colorScheme.surface,
+                  child: Center(child: Text("First")),
+                  elevation: 3,
+                ),
+                Card(
+                  color: Theme.of(context).colorScheme.surface,
+                  child: Center(child: const Text('Second')),
+                  elevation: 3,
+                ),
+                Card(
+                  color: Theme.of(context).colorScheme.surface,
+                  child: Center(child: const Text('Third')),
+                  elevation: 3,
+                ),
+                Card(
+                  color: Theme.of(context).colorScheme.surface,
+                  child: Center(child: const Text('Fourth')),
+                  elevation: 3,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
